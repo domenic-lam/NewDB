b2581a91f74744d5c1abb0695a7fd75d86ad2027
Ely Lam <eydelamb14@gmail.com>
2020-11-02 22:17:01 -0800

    deploy test 2

--- app.js
+++ app.js
@@ -12,7 +12,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "front/build")));
+// app.use(express.static("front/build"));
+app.use(express.static(path.join(__dirname, "./front/build")));
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "./front/build", "index.html"));
+});
 
 app.use("/", indexRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}.`);
+});
 
 module.exports = app;
